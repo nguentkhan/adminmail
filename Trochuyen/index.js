47cf50de6edb15ce1f32b38d12ed6031ac7267fb
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-08-29 15:00:44 +0300

    Update index.js

--- Trochuyen/index.js
+++ Trochuyen/index.js
@@ -31,11 +31,4 @@ function sendMessage(e) {
   });
 }
 
-fetchChat.on("child_added", function (snapshot) {
-  const messages = snapshot.val();
-  const message = `<li class=${
-    username === messages.username ? "sent" : "receive"
-  }><span>${messages.username}: </span>${messages.message}</li>`;
-  // append the message on the page
-  document.getElementById("messages").innerHTML += message;
-});
+
