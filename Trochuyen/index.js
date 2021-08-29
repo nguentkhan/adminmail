018352d2fda2f65e9f24250941dd10717125e988
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-08-29 14:39:38 +0300

    Create index.js

---
+++ Trochuyen/index.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdjSm3UhHAT0YmakG-MKjfsbCFF3BTnnk",
+    authDomain: "trochuyen-67032.firebaseapp.com",
+    projectId: "trochuyen-67032",
+    storageBucket: "trochuyen-67032.appspot.com",
+    messagingSenderId: "318150959454",
+    appId: "1:318150959454:web:eca2204ac8c9f2355befcf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
