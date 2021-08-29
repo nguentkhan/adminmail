14204c145c3f483817f3f2c483c507e1055423ef
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-08-29 14:53:03 +0300

    Update index.js

--- Trochuyen/index.js
+++ Trochuyen/index.js
@@ -7,3 +7,33 @@ var firebaseConfig = {
     appId: "1:318150959454:web:eca2204ac8c9f2355befcf"
   };
 firebase.initializeApp(firebaseConfig);
+function sendMessage(e) {
+  e.preventDefault();
+
+  // get values to be submitted
+  const timestamp = Date.now();
+  const messageInput = document.getElementById("message-input");
+  const message = messageInput.value;
+
+  // clear the input box
+  messageInput.value = "";
+
+  //auto scroll to bottom
+  document
+    .getElementById("messages")
+    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+  // create db collection and send in the data
+  db.ref("messages/" + timestamp).set({
+    username,
+    message,
+  });
+}
+fetchChat.on("child_added", function (snapshot) {
+  const messages = snapshot.val();
+  const message = `<li class=${
+    username === messages.username ? "sent" : "receive"
+  }><span>${messages.username}: </span>${messages.message}</li>`;
+  // append the message on the page
+  document.getElementById("messages").innerHTML += message;
+});
