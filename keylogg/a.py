cf635e06cfcb1e96346670dfe77a651883b58afb
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-08-31 10:28:34 +0300

    Update a.py

--- keylogg/a.py
+++ keylogg/a.py
@@ -1,1 +1,13 @@
-print(Hello world :))))
+import smtplib
+
+sender_email=""
+rec_email=""
+password=input (str("please enter your password: "])
+message="Hey, this was sent using python"
+
+sever=smtplib.SMTP('smtp.gmail.com', 587)
+server starrttls()
+server.login(sender_email, password)
+print("Login success") 
+server.sendmail(sender_email, rec_email, messenge)
+print("Email has been sent to", rec_email)
